84855bc2eff8630b0be9fa08af5f6d31e2b152fb
Alec Davidson <davidson9076@gmail.com>
2018-06-22 15:16:36 -0400

    change lowercase week

--- client/components/index.js
+++ client/components/index.js
@@ -1,4 +1,4 @@
 export {default as Calendar} from './calendar.jsx'
 export {default as EventForm} from './eventForm.jsx'
 export {default as Day} from './day.jsx'
-export {default as Week} from './Week.jsx'
+export {default as Week} from './week.jsx'
